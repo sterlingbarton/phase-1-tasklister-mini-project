34a8040120064edc198275bfaa64d31c6c76f4b8
Sterling <sterlingbarton81@gmail.com>
2023-03-25 19:35:14 -0600

    completed lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener("submit", (e) => {
+  e.preventDefault();
+  const input = document.querySelector("#new-task-description");
+  createTask(input);
+  input.value = "";
 });
+});
+
+
+function createTask(todo) {
+  const ul = document.getElementById("tasks");
+  const li = document.createElement("li");
+  li.textContent = todo.value;
+  ul.appendChild(li);
+}
